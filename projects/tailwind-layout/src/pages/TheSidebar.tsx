f895f49a0879872e8fffc1f06d4598b0616e1463
sillypoise <somewhatjose@gmail.com>
2022-07-21 16:11:22 -0500

    Synced The Sidebar in tailwind with vanilla plus added a bit more verbose documentation. Also fixed a line-height default for the body

--- projects/tailwind-layout/src/pages/TheSidebar.tsx
+++ projects/tailwind-layout/src/pages/TheSidebar.tsx
@@ -5,14 +5,61 @@ export function TheSidebar() {
                 <h1>The Sidebar</h1>
                 <p>
                     Justs add the custom <code>sidebar</code> utility class.
-                    Then proceed to customize the sidebar's children with the{" "}
-                    <code>{`basis-[space scale value]`}</code> and the{" "}
-                    <code>{`min-is-[space scale value]`}</code> Tailwind utility
-                    classes.
+                    Then proceed to customize the sidebar's children by
+                    overriding custom properties. Specifically:
                 </p>
+                <ul className="stack">
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--sidebar-gap`}</code>
+                            </strong>{" "}
+                            → adjusts the gap of the actual{" "}
+                            <strong>sidebar</strong> by changing the value of
+                            the <code>{`gap: []rem;`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>theme(spacing.xs)</code>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--sidebar-width`}</code>
+                            </strong>{" "}
+                            → adjusts the width of the actual{" "}
+                            <strong>sidebar</strong> by changing the value of
+                            the <code>{`flex-basis: []rem;`}</code> property.
+                            Its <strong>default value</strong> is{" "}
+                            <code>theme(spacing.5xl)</code>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--sidebar-threshold`}</code>
+                            </strong>{" "}
+                            → adjusts the percentage breaking point threshold of
+                            the sidebar by changing the{" "}
+                            <code>{`min-inline-size: []%`}</code> property. Its{" "}
+                            <strong>default value</strong> is <code>50%</code>{" "}
+                            which means it will break when both sidebar and
+                            content are of equal size
+                        </p>
+                    </li>
+                </ul>
+                <p>
+                    Breaks at <code>50%</code>
+                </p>
+                <div className="sidebar [--sidebar-width:theme(spacing.6xl)] gap-xs | debug">
+                    <div className="box | bg-red-400/80">I am the sidebar</div>
+                    <div className="box | bg-teal-500">
+                        I am the content that has a sidebar and will stack when
+                        I am equal width as the sidebar
+                    </div>
+                </div>
                 <div className="sidebar [--sidebar-width:theme(spacing.6xl)] [--sidebar-threshold:70%] gap-s | debug">
                     <div className="box | bg-red-400/80">I am the sidebar</div>
-                    <div className="box | bg-teal-600">
+                    <div className="box | bg-teal-500">
                         I am the content that has a sidebar and will stack when
                         I am equal width as the sidebar
                     </div>
@@ -23,18 +70,11 @@ export function TheSidebar() {
                     &nbsp;(
                     <code>data-direction="rtl"</code> )
                 </p>
-                <p>
-                    However make sure to flip around the utility classes for the
-                    sidebar to work. Define the <code>flex-basis</code> in the
-                    element supposed to be the sidebar to determine it's length
-                    and define the <code>min-inline-size</code> in the element
-                    containing the content..
-                </p>
                 <div
                     className="sidebar [--sidebar-width:theme(spacing.6xl)] gap-s | debug"
                     data-direction="rtl"
                 >
-                    <div className="box | bg-teal-600">
+                    <div className="box | bg-teal-500">
                         I am the content that has a sidebar and will stack when
                         I am equal width as the sidebar
                     </div>
