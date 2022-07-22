f90e18300040167e9d84d1d04318fa505b25d5d5
sillypoise <somewhatjose@gmail.com>
2022-07-21 19:38:30 -0500

    Finished syncing up The Grid and also fixed line-height inconsistencies

--- projects/tailwind-layout/src/pages/TheGrid.tsx
+++ projects/tailwind-layout/src/pages/TheGrid.tsx
@@ -1,82 +1,110 @@
 export function TheGrid() {
     return (
-        <div className="center">
+        <article className="center">
             <div className="stack">
                 <h1>The Grid</h1>
+                <p>
+                    Justs add the custom <code>{`auto-grid`}</code> class. Then
+                    proceed to customize the cluster's children through
+                    properties. Specifically:
+                </p>
+                <ul className="stack">
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-gap`}</code>
+                            </strong>{" "}
+                            → adjusts the gap of the actual{" "}
+                            <strong>grid</strong> by changing the value of the{" "}
+                            <code>{`gap: []rem;`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`theme(spacing.s)`}</code>
+                        </p>
+                        <ul>
+                            <li>
+                                <strong>Note</strong> you can also just change
+                                the <code>{`gap-[]`}</code> utility class in
+                                tailwind. E.g. <code>{`gap-m)`}</code>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-placement`}</code>
+                            </strong>{" "}
+                            →{" "}
+                            <span style={{ color: "red" }}>
+                                TODO: What does this actually do?
+                            </span>{" "}
+                            ... by changing the first value of the{" "}
+                            <code>{`repeat: (<value>, <value>)`}</code>{" "}
+                            property, which itself is the first value of the{" "}
+                            <code>{`grid-template-columns`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`auto-fill`}</code>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-min-width`}</code>
+                            </strong>{" "}
+                            → adjusts the{" "}
+                            <strong>
+                                minimum width of grid items, i.e. children
+                            </strong>{" "}
+                            inside the <strong>grid</strong> by changing the
+                            first value of the
+                            <code>{`minmax: <value>`}</code> property, which
+                            itself is the first value of the{" "}
+                            <code>{`grid-template-columns`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`16rem`}</code>.
+                        </p>
+                    </li>
+                </ul>
+                <p>Default values</p>
                 <div className="auto-grid">
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        A
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        B
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        C
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        D
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        E
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        F
-                    </div>
+                    <div className="box bg-red-400/80">A</div>
+                    <div className="box bg-teal-500/60">B</div>
+                    <div className="box bg-red-400/80">C</div>
+                    <div className="box bg-teal-500/60">D</div>
+                    <div className="box bg-red-400/80">E</div>
+                    <div className="box bg-teal-500/60">F</div>
                 </div>
                 <hr />
                 <p>
                     Overwrite the minimum width of a grid child by overwriting
                     the
-                    <code>--auto-grid-min:</code> custom property value
+                    <code>--grid-item-min-width:</code> custom property value
                 </p>
                 <p>
-                    <code>--auto-grid-min:30ch</code>
+                    Has a <code>{`gap-2xs`}</code> gap and minimum item width of{" "}
+                    <code>{`--grid-item-min-width: 30ch`}</code>
                 </p>
-                <div className="auto-grid gap-2xs [--auto-grid-min:30ch]">
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        A
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        B
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        C
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        D
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        E
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        F
-                    </div>
+                <div className="auto-grid gap-2xs [--grid-min-width:30ch]">
+                    <div className="box bg-red-400/80">A</div>
+                    <div className="box bg-teal-500/60">B</div>
+                    <div className="box bg-red-400/80">C</div>
+                    <div className="box bg-teal-500/60">D</div>
+                    <div className="box bg-red-400/80">E</div>
+                    <div className="box bg-teal-500/60">F</div>
                 </div>
                 <hr />
                 <p>
-                    <code>--auto-grid-min:10rem</code>
+                    Has a <code>{`gap-2xs)`}</code> gap and minimum item width
+                    of <code>{`--grid-item-min-width: 10rem`}</code>
                 </p>
-                <div className="auto-grid gap-2xs [--auto-grid-min:10rem]">
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        A
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        B
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        C
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        D
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        E
-                    </div>
-                    <div className="box odd:bg-red-500/80 even:bg-teal-600">
-                        F
-                    </div>
+                <div className="auto-grid gap-2xs [--grid-min-width:10rem]">
+                    <div className="box bg-red-400/80">A</div>
+                    <div className="box bg-teal-500/60">B</div>
+                    <div className="box bg-red-400/80">C</div>
+                    <div className="box bg-teal-500/60">D</div>
+                    <div className="box bg-red-400/80">E</div>
+                    <div className="box bg-teal-500/60">F</div>
                 </div>
             </div>
-        </div>
+        </article>
     );
 }
--- projects/vanilla-layout/src/pages/TheGrid.tsx
+++ projects/vanilla-layout/src/pages/TheGrid.tsx
@@ -1,8 +1,71 @@
 export function TheGrid() {
     return (
-        <div className="center">
+        <article className="center">
             <div className="stack">
                 <h1>The Grid</h1>
+                <p>
+                    Justs add the custom <code>{`auto-grid`}</code> class. Then
+                    proceed to customize the cluster's children through
+                    properties. Specifically:
+                </p>
+                <ul className="stack">
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-gap`}</code>
+                            </strong>{" "}
+                            → adjusts the gap of the actual{" "}
+                            <strong>grid</strong> by changing the value of the{" "}
+                            <code>{`gap: []rem;`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`var(--space-s)`}</code>
+                        </p>
+                        <ul>
+                            <li>
+                                <strong>Note</strong> you can also just change
+                                the <code>{`gap: []`}</code> property with
+                                inline styles. E.g.{" "}
+                                <code>{`gap: --var(--space-m)`}</code>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-placement`}</code>
+                            </strong>{" "}
+                            →{" "}
+                            <span style={{ color: "red" }}>
+                                TODO: What does this actually do?
+                            </span>{" "}
+                            ... by changing the first value of the{" "}
+                            <code>{`repeat: (<value>, <value>)`}</code>{" "}
+                            property, which itself is the first value of the{" "}
+                            <code>{`grid-template-columns`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`auto-fill`}</code>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>
+                                <code>{`--grid-min-width`}</code>
+                            </strong>{" "}
+                            → adjusts the{" "}
+                            <strong>
+                                minimum width of grid items, i.e. children
+                            </strong>{" "}
+                            inside the <strong>grid</strong> by changing the
+                            first value of the
+                            <code>{`minmax: <value>`}</code> property, which
+                            itself is the first value of the{" "}
+                            <code>{`grid-template-columns`}</code> property. Its{" "}
+                            <strong>default value</strong> is{" "}
+                            <code>{`16rem`}</code>.
+                        </p>
+                    </li>
+                </ul>
+                <p>Default values</p>
                 <div className="auto-grid">
                     <div className="box bg-red">A</div>
                     <div className="box bg-teal">B</div>
@@ -18,11 +81,18 @@ export function TheGrid() {
                     <code>--grid-item-min-width:</code> custom property value
                 </p>
                 <p>
-                    <code>--grid-item-min-width:20ch</code>
+                    Has a <code>{`--grid-gap: var(--space-2xs)`}</code> gap and
+                    minimum item width of{" "}
+                    <code>{`--grid-item-min-width: 30ch`}</code>
                 </p>
                 <div
-                    className="auto-grid gap-2xs []"
-                    // style="--grid-item-min-width: 20ch;"
+                    className="auto-grid"
+                    style={
+                        {
+                            "--grid-gap": "var(--space-2xs)",
+                            "--grid-min-width": "30ch",
+                        } as React.CSSProperties
+                    }
                 >
                     <div className="box bg-red">A</div>
                     <div className="box bg-teal">B</div>
@@ -33,9 +103,19 @@ export function TheGrid() {
                 </div>
                 <hr />
                 <p>
-                    <code>--grid-item-min-width:10rem</code>
+                    Has a <code>{`--grid-gap: var(--space-2xs)`}</code> gap and
+                    minimum item width of{" "}
+                    <code>{`--grid-item-min-width: 10rem`}</code>
                 </p>
-                <div className="auto-grid gap-2xs [--grid-item-min-width:10rem]">
+                <div
+                    className="auto-grid"
+                    style={
+                        {
+                            "--grid-gap": "var(--space-2xs)",
+                            "--grid-min-width": "10rem",
+                        } as React.CSSProperties
+                    }
+                >
                     <div className="box bg-red">A</div>
                     <div className="box bg-teal">B</div>
                     <div className="box bg-red">C</div>
@@ -44,6 +124,6 @@ export function TheGrid() {
                     <div className="box bg-teal">F</div>
                 </div>
             </div>
-        </div>
+        </article>
     );
 }
